4d7b75f5cc910c385f73aba858e0f19a7d19bf1d
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-09-10 23:18:14 +0530

    Add files via upload

---
+++ P3ReverseArray.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int[] reverseArray(int[] arr) {
+		int start = 0;
+        int end = arr.length-1;
+        while(start<end)
+        {
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+        
+        start++;
+        end--;
+        }
+        return arr;
+    }
+}
