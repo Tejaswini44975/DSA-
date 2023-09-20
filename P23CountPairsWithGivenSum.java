61e580a2a2996c8c1a0b72f30fb18eca1c3e7f5d
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-09-20 17:56:42 +0530

    Add files via upload

---
+++ P23CountPairsWithGivenSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        int count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]+arr[j]==k)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
