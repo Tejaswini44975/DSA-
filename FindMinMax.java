48c5845dd4ea34238f7bcc0b64fe1332d6dde9f6
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-09-22 22:58:20 +0530

    Add files via upload

---
+++ FindMinMax.java
@@ -0,0 +1,27 @@
+class Compute 
+{
+    static Pair getMinMax(long a[], long n)  
+    {
+        
+          if (n == 0) {
+            return new Pair(-1, -1); 
+        }
+
+        long min = a[0];
+        long max = a[0];
+
+        for (int i = 1; i < n; i++) 
+        {
+            if (a[i] < min)
+            {
+                min = a[i]; 
+            }
+            if (a[i] > max) 
+            {
+                max = a[i];
+            }
+        }
+
+        return new Pair(min, max);
+    }
+}
