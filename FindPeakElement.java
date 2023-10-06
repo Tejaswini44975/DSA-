d8ee1ce54d00e6f3d9d1bfa2d3bb9cc339363dc3
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-10-06 11:53:38 +0530

    Add files via upload

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,27 +1,25 @@
-
+class Solution {
     public int findPeakElement(int[] nums) {
         int n=nums.length;
-        int left=0;
-       int right=n-1;
-       int mid=0;
-     
-       while(left<=right)
-       {
-            mid=left+(right-left)/2;
-           if((mid==0 || nums[mid-1]<nums[mid]) && (mid==n-1 || nums[mid]>nums[mid+1]))
-           {
-               return mid;
-           }
-           else if(mid>0 && nums[mid-1]>nums[mid])
-           {
-               right=mid-1;
-           }
-           else
-           {
-               left=mid+1;
-           }
-       }
-       return -1;
-       
+        int start=0;
+        int end=n-1;
+        int mid=0;
+        while(start<=end)
+        {
+             mid=start+(end-start)/2;
+            if((mid==0 || nums[mid-1]<nums[mid]) && (mid==n-1 || nums[mid]>nums[mid+1]))
+            {
+                return mid;
+            }
+            else if(mid>0 && nums[mid-1]>nums[mid])
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return -1;
     }
-} 
+}
