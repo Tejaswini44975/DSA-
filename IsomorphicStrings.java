8a8923916925b1000f7aab78ec1663f236e238da
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-10-12 09:44:23 +0530

    Add files via upload

---
+++ IsomorphicStrings.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length())
+      {
+          return false;
+      }  
+      Map<Character,Character> mp = new HashMap<>();
+      for(int i=0;i<s.length();i++)
+     {
+
+      char original=s.charAt(i);
+      char replacement=t.charAt(i);
+      
+      if(!mp.containsKey(original))
+      {
+          if(!mp.containsValue(replacement))
+          {
+              mp.put(original,replacement);
+          }
+          else
+          {
+              return false;
+          }
+      }
+      else
+      {
+          char mappedCharacter=mp.get(original);
+          if(mappedCharacter!=replacement)
+          {
+              return false;
+          }
+      }
+     }
+      return true;
+    }
+}
