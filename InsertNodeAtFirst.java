785d15f500da730881b27d26020257ccee3c6651
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-10-18 23:10:15 +0530

    Add files via upload

---
+++ InsertNodeAtFirst.java
@@ -0,0 +1,38 @@
+class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtFirst(Node list, int newValue) {
+        Node node=new Node(newValue);
+        node.next=list;
+        list=node;
+        return list;
+    }
+}
