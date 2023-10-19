2f933cca679ae9a006d1586dbb0d541b21714e9e
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-10-19 15:31:32 +0530

    Add files via upload

---
+++ DeleteLastNodeDLL.java
@@ -0,0 +1,46 @@
+class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node deleteLastNode(Node head) {
+        if(head.next==null)
+        {
+            return null;
+        }
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.prev.next=null;
+        return head;
+
+    }
+}
