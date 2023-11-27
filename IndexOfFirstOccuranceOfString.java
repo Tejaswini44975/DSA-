c58edaef04fe620ad83a5a607c54b4b618e4a53b
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-11-27 22:34:26 +0530

    Create IndexOfFirstOccuranceOfString.java

---
+++ IndexOfFirstOccuranceOfString.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        int hayLength = haystack.length();
+        int needleLength = needle.length();
+
+        for (int i = 0; i <= hayLength - needleLength; i++) {
+            if (haystack.substring(i, i + needleLength).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
