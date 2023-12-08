5957ad1ec813758785c4ca78005dfabe1827de2f
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-12-08 22:12:17 +0530

    Create InorderTraversal.java

---
+++ BinaryTrees/InorderTraversal.java
@@ -0,0 +1,22 @@
+class Node {
+    int data;
+    Node left, right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    static void inOrderTrav(Node curr, ArrayList < Integer > inOrder) 
+  {
+        if (curr == null)
+            return;
+
+        inOrderTrav(curr.left, inOrder);
+        inOrder.add(curr.data);
+        inOrderTrav(curr.right, inOrder);
+    }
+}
