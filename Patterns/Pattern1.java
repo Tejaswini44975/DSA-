c260edf2322b37d9c1bc2f2de64aa073fffabd99
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-02-10 10:30:48 +0530

    Create Pattern1.java

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,22 @@
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* * *
+* * * 
+
+public class Solution
+  {
+    public static void nForest(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
