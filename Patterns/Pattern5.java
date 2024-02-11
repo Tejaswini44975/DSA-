fc48a949814db6999a0e27befc558eb8ebb855a7
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-02-11 23:14:41 +0530

    Create Pattern5.java

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,21 @@
+/*
+  
+* * *
+* *
+*
+  
+/*
+
+public class Solution {
+    public static void seeding(int n)
+     {
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
