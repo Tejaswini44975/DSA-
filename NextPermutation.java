ff0360fc48021062cd26d944452e426a744b6011
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-02-22 22:56:20 +0530

    Update NextPermutation.java

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,26 +1,24 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length; // size of the array.
+        int n = nums.length; 
 
         // Step 1: Find the break point:
         int ind = -1; // break point
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                // index i is the break point
+                
                 ind = i;
                 break;
             }
         }
 
-        // If break point does not exist:
+        
         if (ind == -1) {
-            // reverse the whole array:
+            
             reverse(nums, 0, n - 1);
             return;
         }
 
-        // Step 2: Find the next greater element
-        //         and swap it with arr[ind]:
 
         for (int i = n - 1; i > ind; i--) {
             if (nums[i] > nums[ind]) {
@@ -31,7 +29,6 @@ class Solution {
             }
         }
 
-        // Step 3: reverse the right half:
         reverse(nums, ind + 1, n - 1);
     
     }
