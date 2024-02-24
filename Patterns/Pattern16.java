4ef4e5d9b43c9ebf4fc2576c21e9597f10995be7
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-02-24 18:41:55 +0530

    Create Pattern16.java

---
+++ Patterns/Pattern16.java
@@ -0,0 +1,24 @@
+/* Input: ‘N’ = 3
+
+Output: 
+A
+B B
+C C C  */
+
+
+
+
+
+public class Solution {
+    public static void alphaRamp(int n) {
+        for(int i=1;i<=n;i++)
+        {
+            char ch=(char)('A'+i-1);
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
