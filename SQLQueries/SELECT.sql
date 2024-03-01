42e3b41447b995b4232d432ffb5385e875a63953
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-01 17:18:16 +0530

    Update SELECT.sql

--- SQLQueries/SELECT.sql
+++ SQLQueries/SELECT.sql
@@ -1,3 +1,13 @@
 /*Insert the missing statement to get all the columns from the Customers table. */
 
 SELECT * FROM Customers;
+
+/*Write a statement that will select the City column from the Customers table. */
+
+SELECT City FROM Customers;
+
+/* Select all the different values from the Country column in the Customers table. */
+
+SELECT DISTINCT Country FROM Customers;
+
+SELECT * FROM Customers WHERE City= 'Berlin';
