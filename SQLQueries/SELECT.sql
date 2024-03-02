f17ae8dc736f7c376e3419e9a3c88eb9332fb432
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-02 23:43:51 +0530

    Update SELECT.sql

--- SQLQueries/SELECT.sql
+++ SQLQueries/SELECT.sql
@@ -13,3 +13,7 @@ SELECT DISTINCT Country FROM Customers;
 /*Select all records where the City column has the value "Berlin". */
 
 SELECT * FROM Customers WHERE City= 'Berlin';
+
+/*Select all records where the CustomerID column has the value 32. */
+
+SELECT * FROM Customers WHERE CustomerID = 32;
