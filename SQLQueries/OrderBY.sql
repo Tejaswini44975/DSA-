10848ae21c8dd11f1cfaa9f07a43d254a712c71a
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-04 23:05:46 +0530

    Update OrderBY.sql

--- SQLQueries/OrderBY.sql
+++ SQLQueries/OrderBY.sql
@@ -1,3 +1,15 @@
 /* Select all records from the Customers table, sort the result alphabetically by the column City. */
 
 SELECT * FROM Customers ORDER BY City;
+
+/* Select all records from the Customers table, sort the result reversed alphabetically by the column City. */
+
+SELECT * FROM Customers ORDER BY City DESC;
+
+/* Select all records from the Customers table, sort the result alphabetically, first by the column Country, then, by the column City. */
+
+SELECT * FROM Customers ORDER BY Country,City;
+
+/* Select all records where the City column has the value 'Berlin' and the PostalCode column has the value '12209'.  */
+
+SELECT * FROM Customers WHERE City = 'Berlin' AND PostalCode = '12209';
