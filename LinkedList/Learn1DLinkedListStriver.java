4b197d20b0cd3512ece594c1eaaab28df5478a9f
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-09 22:51:43 +0530

    Update Learn1DLinkedListStriver.java

--- LinkedList/Learn1DLinkedListStriver.java
+++ LinkedList/Learn1DLinkedListStriver.java
@@ -63,3 +63,16 @@ public class Solution {
         return list;
     }
 }
+
+public class Solution {
+    public static int length(Node head){
+        Node temp=head;
+        int count=0;
+        while(temp!=null)
+        {
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
