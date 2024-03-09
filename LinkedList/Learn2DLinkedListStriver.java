9096d2503e884cab340457272ad461c945b5e40f
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-09 23:50:44 +0530

    Create Learn2DLinkedListStriver.java

---
+++ LinkedList/Learn2DLinkedListStriver.java
@@ -0,0 +1,52 @@
+class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtTail(Node list, int K) {
+        Node node = new Node(K);
+
+        if(list==null)
+        {
+            return node;
+        }
+        
+        Node temp=list;
+
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=node;
+        node.prev=temp;
+
+        return list;
+    }
+}
+
