49f9c208d22d8361040bf2365112af6d00e4080c
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2024-03-17 23:50:16 +0530

    Create setMatrixZero.java

---
+++ Arrays/setMatrixZero.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;    // Number of rows
+        int m = matrix[0].length; // Number of columns
+    
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1)
+                 {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
